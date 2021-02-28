00c7002d7807b8292cf8c5c8962806a921db8f65
Sadanala <ssadanala@deloitte.com>
2021-02-28 15:42:42 +0530

    @ConfigurationProperties changes

---
+++ src/main/java/com/ss/microservices/configuration/Configuration.java
@@ -0,0 +1,26 @@
+package com.ss.microservices.configuration;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(value = "limits-service")
+public class Configuration {
+	
+	private Integer min;
+	private Integer max;
+	
+	public Integer getMin() {
+		return min;
+	}
+	public void setMin(Integer min) {
+		this.min = min;
+	}
+	public Integer getMax() {
+		return max;
+	}
+	public void setMax(Integer max) {
+		this.max = max;
+	}
+
+}
--- src/main/java/com/ss/microservices/controller/LimitsController.java
+++ src/main/java/com/ss/microservices/controller/LimitsController.java
@@ -1,15 +1,21 @@
 package com.ss.microservices.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ss.microservices.configuration.Configuration;
 import com.ss.microservices.dto.Limits;
 
 @RestController
 public class LimitsController {
 
+	@Autowired
+	private Configuration config;
+
 	@GetMapping(value = "/limits")
 	public Limits limits() {
-		return new Limits(Integer.MIN_VALUE, Integer.MAX_VALUE);
+		// return new Limits(Integer.MIN_VALUE, Integer.MAX_VALUE);
+		return new Limits(config.getMin(), config.getMax());
 	}
 }
